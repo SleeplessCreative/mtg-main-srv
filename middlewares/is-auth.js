9ece120ad6aed3bb79893d219956e320aae8527f
Taufik Algi F <taufik.algi99@gmail.com>
2021-10-06 18:03:27 +0700

    fix: delete return in is-auth middleware and move error middleware

--- middlewares/is-auth.js
+++ middlewares/is-auth.js
@@ -21,9 +21,5 @@ module.exports = (req, res, next) => {
     throw error;
   }
   req.userId = decodedToken.userId;
-
-  return res.status(200).json({
-    message: "Authenticated",
-  });
   next();
 };
